a11e24a6431699144954ee8e134a203b424a980a
Zaidan Naufal <zaidannaufal7@gmail.com>
2020-11-19 20:00:57 +0700

    edit  main menu dikit

--- src/main.c
+++ src/main.c
@@ -9,10 +9,12 @@
 int main(){
     boolean gamestart = false; // kalo true berarti game jalan
     Kata input; //cuma dipake si tab katanya doang lenght nya belum dipake
+    char nama[100];
     PERINTAH per;
     MATRIKS map;
     POINT playerpos;
-    printf("loading perintah....");
+
+    
     per = siapkan_perintah();
     map = renderMap();
     initiatePlayerPosition(&map,&playerpos);
@@ -24,20 +26,43 @@ int main(){
     if(compare_string(input.TabKata,Command(per,0))){ //kalo dimasukkin command 1 bakal gamestart
         gamestart= true;
     }
+    printf("Memulai permainan baru...\n");
+    printf("Masukkan Nama: ");
+    scanf("%s", nama);
+    
     while (gamestart)
     {
+        printf("\n");
         TulisMATRIKS(map);
         printf("\n");
-        printf("ini game\n");
+        printf("Legend:\n");
+        printf("A = Antrian\n");
+        printf("P = Player\n");
+        printf("W = Wahana\n");
+        printf("O = Office\n");
+        printf("<, ^, >, V = Gerbang\n");
+        printf("\n");
+
+        printf("Name: %s\n",nama);
+        printf("Money:\n");
+        printf("Current Time: \n");
+        printf("Opening Time: \n");
+        printf("Time Remaining: \n");
+        printf("Total aksi yang akan dilakukan: \n");
+        printf("Total waktu yang dibutuhkan: \n");
+        printf("Total uang yang dibutuhkan: \n");
+        
         scanf("%s",input.TabKata);
         // printf("%c %c",input.TabKata[0],input.TabKata[1]);
         if(compare_string(input.TabKata,Command(per,2))){ //kalo dimasukkin command 2 (exit) bakal ke exit
             gamestart = false;
         } else if (compare_string(input.TabKata,Command(per,16))||compare_string(input.TabKata,Command(per,17))
-        || compare_string(input.TabKata,Command(per,18)) || compare_string(input.TabKata,Command(per,19))) // ini liat commandnya di fungsi siapkan perintah yah
+                   || compare_string(input.TabKata,Command(per,18)) || compare_string(input.TabKata,Command(per,19))) // ini liat commandnya di fungsi siapkan perintah yah
         {
             movePlayer(&map,&playerpos,input.TabKata[0]);
         }
+        
+
         
         // }else if(isalpha(input.TabKata)) {
         //     movePlayer(&map,&playerpos,input.TabKata[0]);
