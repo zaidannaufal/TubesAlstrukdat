88a9e37b0014b70258353849e49532e4a6887b10
Zaidan Naufal <zaidannaufal7@gmail.com>
2020-11-29 16:23:35 +0700

    up tambahin bbs

--- src/upgrade.c
+++ src/upgrade.c
@@ -16,62 +16,62 @@ int TipeRootWahana(char* NamaWahana, ListBangunan * LB)
 }
 
 
-// void Upgrade(ListBangunan * LB, BinTree* BT, BAHAN bb,BAHAN *bbs, int * Bill, int * Aksi, int CurrentMoney, int * TambahanWaktu)
-// {
-// 	printf("Mau upgrade apa?\nList:\n");
-// 	addressbangunan Current = First(*LB);
-// 	while(Current != NULL) // Print semua wahana yang dipunya
-// 	{
-// 		printf("- ");
-// 		puts(nama(Current));
-// 		Current = Next(Current);
-// 	}
-// 	char NamaBangunan[50];
-// 	int idxNBangunan = 0;
-//     do {
-//         scanf("%c", NamaBangunan + idxNBangunan);
-//     } while (NamaBangunan[idxNBangunan++] != '\n');
-//     NamaBangunan[--idxNBangunan] = '\0';
-// 	int TipeRoot = TipeRootWahana(NamaBangunan,LB); //Nyari Root Wahana Yang Dimaksud
-// 	addressbangunan BangunanTujuan = First(*LB);
-// 	while (BangunanTujuan != NULL && (strcmp(nama(BangunanTujuan),NamaBangunan)) != 0)
-// 	{
-// 		BangunanTujuan = Next(BangunanTujuan);
-// 	}
-// 	int Keuangan = CurrentMoney - (*Bill) - HUpgrade(BangunanTujuan);
-// 	if (Keuangan >= 0)
-// 	{
-// 		BinTree Root = BT[TipeRoot]; //Root Wahananya dalam bentukk bintree
-// 		if (history(BangunanTujuan) == 0 && BBcukup(bb,*bbs,((Left(BT[TipeRoot]))->info).resource.resource))
-// 		{
-// 			(*Bill) += HUpgrade(BangunanTujuan);
-// 			//Belum bahan bangunan
-// 			Root = Left(Root);
-// 			Harga(BangunanTujuan) = harga(Root);
-// 			durasibangunan(BangunanTujuan) = durasibt(Root);
-// 			KapasitasBang(BangunanTujuan) = kapasitas(Root);
-// 			HUpgrade(BangunanTujuan) = HUpgrade(Root);
-// 			(*TambahanWaktu)+=90;
-// 			(*Aksi)++;
-// 			history(BangunanTujuan)++;
-// 		}else if (history(BangunanTujuan) == 1&& BBcukup(bb,*bbs,((Left(Left(BT[TipeRoot])))->info).resource.resource))
-// 		{
-// 			(*Bill) += HUpgrade(BangunanTujuan);
-// 			//Belum bahan bangunan
-// 			Root = Left(Left(Root));
-// 			Harga(BangunanTujuan) = harga(Root);
-// 			durasibangunan(BangunanTujuan) = durasibt(Root);
-// 			KapasitasBang(BangunanTujuan) = kapasitas(Root);
-// 			HUpgrade(BangunanTujuan) = HUpgrade(Root);
-// 			(*TambahanWaktu)+=90;
-// 			(*Aksi)++;
-// 			history(BangunanTujuan)++;
-// 		}else if (history(BangunanTujuan) == 2)
-// 		{
-// 			printf("Tidak bisa diupgrade lagi, wahan sudah mencapai tingkatan upgrade maksimum\n");
-// 		}
-// 	}else printf("Uang tidak mencukupi!\n");
-// }
+void Upgrade(ListBangunan * LB, BinTree* BT, BAHAN bb,BAHAN *bbs, int * Bill, int * Aksi, int CurrentMoney, int * TambahanWaktu)
+{
+	printf("Mau upgrade apa?\nList:\n");
+	addressbangunan Current = First(*LB);
+	while(Current != NULL) // Print semua wahana yang dipunya
+	{
+		printf("- ");
+		puts(nama(Current));
+		Current = Next(Current);
+	}
+	char NamaBangunan[50];
+	int idxNBangunan = 0;
+    do {
+        scanf("%c", NamaBangunan + idxNBangunan);
+    } while (NamaBangunan[idxNBangunan++] != '\n');
+    NamaBangunan[--idxNBangunan] = '\0';
+	int TipeRoot = TipeRootWahana(NamaBangunan,LB); //Nyari Root Wahana Yang Dimaksud
+	addressbangunan BangunanTujuan = First(*LB);
+	while (BangunanTujuan != NULL && (strcmp(nama(BangunanTujuan),NamaBangunan)) != 0)
+	{
+		BangunanTujuan = Next(BangunanTujuan);
+	}
+	int Keuangan = CurrentMoney - (*Bill) - HUpgrade(BangunanTujuan);
+	if (Keuangan >= 0)
+	{
+		BinTree Root = BT[TipeRoot]; //Root Wahananya dalam bentukk bintree
+		if (history(BangunanTujuan) == 0 && BBcukup(bb,*bbs,((Left(BT[TipeRoot]))->info).resource.resource))
+		{
+			(*Bill) += HUpgrade(BangunanTujuan);
+			//Belum bahan bangunan
+			Root = Left(Root);
+			Harga(BangunanTujuan) = harga(Root);
+			durasibangunan(BangunanTujuan) = durasibt(Root);
+			KapasitasBang(BangunanTujuan) = kapasitas(Root);
+			HUpgrade(BangunanTujuan) = HUpgrade(Root);
+			(*TambahanWaktu)+=90;
+			(*Aksi)++;
+			history(BangunanTujuan)++;
+		}else if (history(BangunanTujuan) == 1&& BBcukup(bb,*bbs,((Left(Left(BT[TipeRoot])))->info).resource.resource))
+		{
+			(*Bill) += HUpgrade(BangunanTujuan);
+			//Belum bahan bangunan
+			Root = Left(Left(Root));
+			Harga(BangunanTujuan) = harga(Root);
+			durasibangunan(BangunanTujuan) = durasibt(Root);
+			KapasitasBang(BangunanTujuan) = kapasitas(Root);
+			HUpgrade(BangunanTujuan) = HUpgrade(Root);
+			(*TambahanWaktu)+=90;
+			(*Aksi)++;
+			history(BangunanTujuan)++;
+		}else if (history(BangunanTujuan) == 2)
+		{
+			printf("Tidak bisa diupgrade lagi, wahan sudah mencapai tingkatan upgrade maksimum\n");
+		}
+	}else printf("Uang tidak mencukupi!\n");
+}
 
 void printwahana(addressbangunan wahana){
 	printf("nama wahana: ");
