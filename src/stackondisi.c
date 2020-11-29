5a08ad20e6696d481959fe01ed5664fc8a64e0c1
Zaidan Naufal <zaidannaufal7@gmail.com>
2020-11-29 10:55:31 +0700

    edit selector kondisi buat undo

--- src/stackondisi.c
+++ src/stackondisi.c
@@ -1,28 +1,32 @@
 #include "stackondisi.h"
 
-void CreateEmptyKondisi (Kondisi *S){
+void CreateEmptyKondisi (StackCond *S){
     Top(*S) = NilStack;
-    waktuStack(*S) = 0;
-    bahanStack(*S).wood = 100000;
-    bahanStack(*S).gold = 100000;
-    bahanStack(*S).stone = 100000;
-    uangStack(*S) = 0;
-    WilayahStack(*S) = -1;
+    // waktuStack(*S) = 0;
+    // bahanStack(*S).wood = 100000;
+    // bahanStack(*S).gold = 100000;
+    // bahanStack(*S).stone = 100000;
+    // uangStack(*S) = 0;
+    // WilayahStack(*S) = -1;
 }
-void Push (Stack * S, Kondisi X){
+void Push (StackCond *S, Kondisi X){
     Top(*S)++;
-    waktuStack(*S) = waktuStack(X);
-    bahanStack(*S) = bahanStack(X);
-    WilayahStack(*S) = WilayahStack(X);
-    uangStack(*S) = uangStack(X);
-    CopyMATRIKS(MapStack(X), MapStack(*S));
+    WaktuCond(Cond(*S)) = WaktuCond(X);
+    wood(BahanCond(Cond(*S))) = wood(BahanCond(X));
+    stone(BahanCond(Cond(*S))) = stone(BahanCond(X));
+    gold(BahanCond(Cond(*S))) = gold(BahanCond(X));
+    WilayahCond(Cond(*S)) = WilayahCond(X);
+    UangCond(Cond(*S)) = UangCond(X);
+    CopyMATRIKS(MapCond(X), &MapCond(Cond(*S)));
 }
 
-void Pop (Stack* S, Kondisi* X){
-    waktuStack(*X) = waktuStack(S);
-    bahanStack(*X) = bahanStack(S);
-    WilayahStack(*X) = WilayahStack(S);
-    uangStack(*X) = uangStack(S);
-    CopyMATRIKS(MapStack(S), MapStack(*X));
+void Pop (StackCond *S,Kondisi *X){
+    WaktuCond((*X))= WaktuCond(Cond(*S));
+    wood(BahanCond((*X))) = wood(BahanCond(Cond(*S)));
+    stone(BahanCond((*X))) = stone(BahanCond(Cond(*S)));
+    gold(BahanCond((*X))) = gold(BahanCond(Cond(*S)));
+    WilayahCond((*X)) = WilayahCond(Cond(*S));
+    UangCond((*X)) = UangCond(Cond(*S));
+    CopyMATRIKS(MapCond(Cond(*S)), &MapCond((*X)));
     Top(*S)--;
 }
--- src/stackondisi.h
+++ src/stackondisi.h
@@ -1,13 +1,17 @@
-#include "graphmap.h"
-#include "boolean.h"
-#include "build.h"
+// #include "graphmap.h"
+// #include "boolean.h"
+// #include "build.h"
+#include "upgrade.h"
 #define NilStack -1
 #define MaxElStack 20 
+
+#ifndef STACKTKONDISI_H
+#define STACKTKONDISI_H
 
 typedef struct {
     Kondisi T[MaxElStack];
     int TOP;
-} addressTOP;
+} StackCond;
 
 typedef struct {
     int waktu;
@@ -18,10 +22,14 @@ typedef struct {
 } Kondisi;
 
 #define Top(S) (S).TOP
-#define waktuStack(S) (S.T[Top]).waktu 
-#define bahanStack(S) (S.T[Top]).bahan
-#define MapStack(S) (S.T[Top]).Map
-#define WilayahStack(S) (S.T[Top]).Wilayah
-#define uangStack(S) (S.T[Top]).uang
+#define Cond(S) (S).T[(S).TOP]
+#define WaktuCond(K) K.waktu
+#define UangCond(K) K.uang
+#define BahanCond(K) K.bahan
+#define MapCond(K) K.Map
+#define WilayahCond(K) K.Wilayah  
 
+void CreateEmptyKondisi (StackCond *S);
+void Push (StackCond *S, Kondisi X);
+void Pop (StackCond *S, Kondisi *X);
 #endif
